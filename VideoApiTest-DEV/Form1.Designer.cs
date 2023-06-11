4bd578363c3b1dde4a01bd25da7fe2dbaacbe4a8
Daniel <danielftosta@hotmail.com>
2023-06-11 03:20:39 -0300

    Add files via upload

---
+++ VideoApiTest-DEV/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace VideoApiTest_DEV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listITEMS = new System.Windows.Forms.ListBox();
+            this.textofrandomthingy = new System.Windows.Forms.Label();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.videotitle = new System.Windows.Forms.Label();
+            this.type = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listITEMS
+            // 
+            this.listITEMS.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listITEMS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listITEMS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listITEMS.ForeColor = System.Drawing.Color.White;
+            this.listITEMS.FormattingEnabled = true;
+            this.listITEMS.Items.AddRange(new object[] {
+            "LOADING..."});
+            this.listITEMS.Location = new System.Drawing.Point(485, 12);
+            this.listITEMS.Name = "listITEMS";
+            this.listITEMS.Size = new System.Drawing.Size(282, 442);
+            this.listITEMS.TabIndex = 0;
+            this.listITEMS.SelectedIndexChanged += new System.EventHandler(this.listITEMS_SelectedIndexChanged);
+            // 
+            // textofrandomthingy
+            // 
+            this.textofrandomthingy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textofrandomthingy.AutoSize = true;
+            this.textofrandomthingy.ForeColor = System.Drawing.Color.White;
+            this.textofrandomthingy.Location = new System.Drawing.Point(13, 441);
+            this.textofrandomthingy.Name = "textofrandomthingy";
+            this.textofrandomthingy.Size = new System.Drawing.Size(99, 13);
+            this.textofrandomthingy.TabIndex = 2;
+            this.textofrandomthingy.Text = "Made by Daniel176";
+            // 
+            // player
+            // 
+            this.player.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(13, 13);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(466, 293);
+            this.player.TabIndex = 3;
+            // 
+            // videotitle
+            // 
+            this.videotitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.videotitle.AutoSize = true;
+            this.videotitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.videotitle.Location = new System.Drawing.Point(13, 313);
+            this.videotitle.Name = "videotitle";
+            this.videotitle.Size = new System.Drawing.Size(14, 20);
+            this.videotitle.TabIndex = 4;
+            this.videotitle.Text = " ";
+            // 
+            // type
+            // 
+            this.type.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.type.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.type.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.type.ForeColor = System.Drawing.Color.White;
+            this.type.FormattingEnabled = true;
+            this.type.Items.AddRange(new object[] {
+            "Buscando o Herobrine - 1° Temporada",
+            "Buscando o Herobrine - 2° Temporada",
+            "Videos do Vicente",
+            "Videos Aleatorios"});
+            this.type.Location = new System.Drawing.Point(291, 313);
+            this.type.Name = "type";
+            this.type.Size = new System.Drawing.Size(188, 52);
+            this.type.TabIndex = 6;
+            this.type.SelectedIndexChanged += new System.EventHandler(this.type_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(779, 466);
+            this.Controls.Add(this.type);
+            this.Controls.Add(this.videotitle);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.textofrandomthingy);
+            this.Controls.Add(this.listITEMS);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Daniel176.ml Video Player Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listITEMS;
+        private System.Windows.Forms.Label textofrandomthingy;
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.Label videotitle;
+        private System.Windows.Forms.ListBox type;
+    }
+}
+
---
+++ VideoApiTest-DEV/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using AxWMPLib;
+using System.IO;
+
+namespace VideoApiTest_DEV
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> videoUrls; // Declaração do dicionário
+        public Form1()
+        {
+            InitializeComponent(); 
+            videoUrls = new Dictionary<string, string>();
+        }
+
+        public class Result
+        {
+            public string Name { get; set; }
+            // Add other properties as needed
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void listITEMS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listITEMS.SelectedItem != null && listITEMS.SelectedItem is string) // Verifica se há um item selecionado e se é uma string
+            {
+                string selectedVideo = listITEMS.SelectedItem.ToString();
+
+                if (videoUrls.ContainsKey(selectedVideo)) // Verifica se o vídeo está presente no dicionário
+                {
+                    string videoUrl = videoUrls[selectedVideo]; // Obtém a URL com base no nome do vídeo selecionado no dicionário
+
+                    // Manipula o vídeo selecionado
+                    PlayVideo(videoUrl, selectedVideo);
+                }
+            }
+        }
+
+
+        private async void loadVideosList(string type)
+        {
+            string apiUrl = "https://daniel176.ml/api.php?type=" + type;
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string json = await response.Content.ReadAsStringAsync();
+                    List<string> videoUrlsList = JsonConvert.DeserializeObject<List<string>>(json);
+
+                    videoUrls = new Dictionary<string, string>(); // Criação do dicionário
+                    listITEMS.Items.Clear(); // Limpa a ListBox antes de adicionar novos itens
+                    foreach (string url in videoUrlsList)
+                    {
+                        string filename = Path.GetFileName(url);
+                        listITEMS.Items.Add(filename);
+                        videoUrls.Add(filename, url); // Adiciona a URL ao dicionário com o nome do vídeo como chave
+                    }
+                }
+                catch (Exception ex)
+                {
+                    textofrandomthingy.Text = ex.Message;
+                }
+            }
+        }
+
+
+
+        private void PlayVideo(string videoUrl, string videoName)
+        {
+            // Implement your video playback logic here
+            // For example, you can use a media player control or open the video in a separate player application
+            string filename = Path.GetFileName(videoName);
+            player.URL = videoUrl;
+            videotitle.Text = filename;
+            textofrandomthingy.Text = "Loading: "+videoUrl;
+        }
+
+        private void type_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (type.SelectedIndex != -1) // Check if an item is selected
+            {
+                string typea = type.SelectedItem.ToString();
+                loadVideosList(typea);
+            }
+        }
+    }
+}
